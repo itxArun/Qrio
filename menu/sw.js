96c53b0f2983d5d94fb404f5085404f86fb39235
itxArun <thanoslive135@gmail.com>
2026-08-21 10:19:36 +0530

    Update sw.js

--- menu/sw.js
+++ menu/sw.js
@@ -1,1 +1,10 @@
+//V1
+self.addEventListener('install', (e) => {
+    console.log('[NextPlate] Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+    // Bas basic fetch logic taaki browser isko installable PWA maan le
+});
+
 
